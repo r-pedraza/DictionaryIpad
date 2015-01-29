9dc5d1b25171e366b0ee1343c4114c78141203a0
Raúl Pedraza <raulpedrazaleon@gmail.com>
2015-01-29 10:48:47 +0100

    Initial Commit

---
+++ DictionaryIpad/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DictionaryIpad
+//
+//  Created by Raul Pedraza on 29/1/15.
+//  Copyright (c) 2015 Raul Pedraza. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
